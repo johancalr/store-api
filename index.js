25d7a086896da3950d59e1be164f1a840c8f69fb
Johan Camilo Lozano Ramírez <johancalr11@gmail.com>
2024-04-08 08:25:27 -0500

    Port config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { routerApi } = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 // Server initialization
 const app = express();
-const port = 3000;
+const port = process.env.PORT??3000;
 
 app.use(express.json());
 const whiteList = [
