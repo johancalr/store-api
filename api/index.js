1a77ac4bb198e9dc087efc3849454f4fc6ccada9
Johan Camilo Lozano Ramírez <johancalr11@gmail.com>
2024-05-25 14:23:53 -0500

    Customize common routes

--- api/index.js
+++ api/index.js
@@ -8,9 +8,9 @@ const port = process.env.PORT??3000;
 
 app.use(express.json());
 const whiteList = [
-  'http://localhost:8080',
+  //'http://localhost:8080',
   'http://localhost:3000',
-  'http://myapp.co'
+  //'http://myapp.co'
 ];
 const options = {
   origin: (origin, callback) => {
@@ -24,11 +24,17 @@ const options = {
 app.use(cors(options));
 
 // Routes creation
+app.get('/', (req, res) => {
+  res.send('Wellcome to my API');
+});
 app.get('/api', (req, res) => {
   res.send('Hello Word with Express')
 });
 
 routerApi(app);
+app.get('/*', (req, res) => {
+  res.send('There is nothing here');
+});
 app.use(logErrors);
 app.use(ormErrorHandler);
 app.use(boomErrorHandler);
